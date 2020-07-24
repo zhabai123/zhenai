caa6e97a3e9d480b369205ab05bb1db57f5cd860
zhabai <1572135200@qq.com>
2020-07-24 11:58:14 +0800

    zhenai

---
+++ zhenai_web_manager/src/main/java/com/zhenai/controller/goods/UploadController.java
@@ -0,0 +1,32 @@
+package com.zhenai.controller.goods;
+
+import com.aliyun.oss.OSSClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import java.io.IOException;
+import java.util.UUID;
+
+
+@RestController
+
+@RequestMapping("/upload")
+public class UploadController {
+    @Autowired
+    private OSSClient ossClient;
+    @PostMapping("/oss")
+    public String nativeUpload(@RequestParam("file") MultipartFile file,
+                               String folder){
+        String bucketName="zhabai";
+        String fileName=folder+"/"+ UUID.randomUUID()+file.getOriginalFilename();
+        try{
+            ossClient.putObject(bucketName,fileName,file.getInputStream());
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        return "http://"+bucketName+".oss-cn-beijing.aliyuncs.com/"+fileName;
+    }
+}
